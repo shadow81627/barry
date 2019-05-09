f6bc568e3dd14be5b0ed0b179b121c85c3229bbe
Damien Robinson <damien.robinson@xcommedia.com.au>
2019-05-09 13:57:23 +1000

    fix(pagination): [BACREP-22] hide on mobile

--- components/Layout/Navbar/Pagination.vue
+++ components/Layout/Navbar/Pagination.vue
@@ -1,5 +1,5 @@
 <template>
-  <nav class="row my-4 justify-content-center pagination-wrapper">
+  <nav class="row my-4 justify-content-center pagination-wrapper d-none d-md-flex">
     <div v-for="link in $t('pagination')" :key="link.value" class="col-md-auto col-6">
       <b-nav-item :to="localePath(link.value)" link-classes="p-0" active-class="active">
         <i class="fa fa-circle" aria-hidden="true" />
