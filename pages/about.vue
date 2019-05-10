ca65df4ae573bd23c1ee2b8881e2cebea2b95525
Damien Robinson <damien.robinson@xcommedia.com.au>
2019-05-10 15:31:56 +1000

    feat(about): add card 5

--- pages/about.vue
+++ pages/about.vue
@@ -1,28 +1,19 @@
 <template>
   <div>
+    <!-- card1 -->
     <div class="row py-5">
       <layerCard src="/images/Layer919.png" reverse>
         <div class="intro">
-          <h2 class="pb-4" v-html="$t('about.card1_title')" />
+          <h2 class="pb-4" v-html="$t('about.card1.title')" />
           <p class="sub-heading">
-            {{ $t('about.card1_intro') }}
+            {{ $t('about.card1.intro') }}
           </p>
-          <p>
-            {{ $t('about.card1_body_line1') }}
-          </p>
-          <p>
-            {{ $t('about.card1_body_line2') }}
-          </p>
-          <p>
-            {{ $t('about.card1_body_line3') }}
-          </p>
-          <p>
-            {{ $t('about.card1_body_line4') }}
-          </p>
+          <p v-for="item in $t('about.card1.body')" :key="item" v-html="item" />
         </div>
       </layerCard>
     </div>
 
+    <!-- card2 -->
     <TwoByOne class="py-5">
       <template #column-1>
         <div
@@ -30,27 +21,47 @@
           style="background-image: url('/images/fader5.png');background-size: cover;"
         >
           <h2 class="display-3 text-light">
-            {{ $t('about.card2_title') }}
+            {{ $t('about.card2.title') }}
           </h2>
         </div>
       </template>
       <template #column-2>
-        <p class="pb-2" style="font-size:1.5rem;" v-html="$t('about.card2_intro')" />
-        <p class="pb-2" v-html="$t('about.card2_body_line1')" />
-
+        <div class="intro">
+          <p class="pb-2 sub-heading" v-html="$t('about.card2.intro')" />
+          <p v-for="item in $t('about.card2.body')" :key="item" v-html="item" class="pb-2" />
+        </div>
         <nuxt-link
           :to="{ path: localePath('entry'), hash: '#page-content' }"
           class="[ btn ] btn-dark [ hero__cta ] shadow"
-          >{{ $t('about.card2_cta') }}</nuxt-link
+          >{{ $t('about.card2.cta') }}</nuxt-link
         >
       </template>
     </TwoByOne>
 
+    <!-- card3 -->
+    <TwoByOne class="py-5" reverse>
+      <template #column-1>
+        <div
+          class="text-center p-5"
+          style="background-image: url('/images/fader5.png');background-size: cover;"
+        >
+          <h2 class="display-3 text-light">
+            {{ $t('about.card3.title') }}
+          </h2>
+        </div>
+      </template>
+      <template #column-2>
+        <div class="intro">
+          <p v-for="item in $t('about.card3.body')" :key="item" v-html="item" class="pb-2" />
+        </div>
+      </template>
+    </TwoByOne>
+
+    <!-- card4 -->
     <div class="row py-5">
       <LayerCard src="/images/Layer783.png">
-        <h2>{{ $t('about.card4_title') }}</h2>
-        <p v-html="$t('about.card4_body_line1')" />
-        <p v-html="$t('about.card4_body_line2')" />
+        <h2>{{ $t('about.card4.title') }}</h2>
+        <p v-for="item in $t('about.card4.body')" :key="item" v-html="item" />
       </LayerCard>
     </div>
 
@@ -59,46 +70,67 @@
         <img class="image-fluid mx-auto d-block mw-100" src="/images/GBRcopy.png" />
       </template>
       <template #column-2>
-        <p style="font-size: 1.25rem;" v-html="$t('about.card4_intro')" />
+        <div class="intro">
+          <p class="sub-heading" v-html="$t('about.card4.intro')" />
+        </div>
 
         <div class="row">
           <div class="col-6">
             <img src="/images/10icon.png" height="42" width="42" />
-            <p v-html="$t('about.card4_list1')" />
+            <p v-html="$t('about.card4.list[1]')" />
           </div>
           <div class="col-6">
             <img src="/images/70MIconcopy.png" height="42" width="42" />
-            <p v-html="$t('about.card4_list2')" />
+            <p v-html="$t('about.card4.list[2]')" />
           </div>
         </div>
 
         <div class="row">
-          <div class="col-12">
+          <div class="col-6">
             <img src="/images/MoonIconcopy.png" height="42" width="42" />
-            <p v-html="$t('about.card4_list3')" />
+            <p v-html="$t('about.card4.list[3]')" />
+          </div>
+          <div class="col-6">
+            <img src="/images/2300KMIconcopy.png" height="42" width="42" />
+            <p v-html="$t('about.card4.list[4]')" />
           </div>
         </div>
 
         <div class="row">
           <div class="col-6">
             <img src="/images/2300KMIconcopy.png" height="42" width="42" />
-            <p v-html="$t('about.card4_list4')" />
+            <p v-html="$t('about.card4.list[5]')" />
           </div>
           <div class="col-6">
             <img src="/images/ECOIconcopy.png" height="42" width="42" />
-            <p v-html="$t('about.card4_list5')" />
+            <p v-html="$t('about.card4.list[6]')" />
           </div>
         </div>
       </template>
     </TwoByOne>
 
-    <div class="row">
-      <div class="col-12 text-center">
+    <!-- card5 -->
+    <TwoByOne class="py-5">
+      <template #column-1>
+        <div
+          class="text-center p-5"
+          style="background-image: url('/images/fader5.png');background-size: cover;"
+        >
+          <h2 class="display-3 text-light">
+            {{ $t('about.card2.title') }}
+          </h2>
+        </div>
+      </template>
+      <template #column-2>
+        <div class="intro">
+          <h2 v-html="$t('about.card5.title')" />
+          <p v-for="item in $t('about.card5.body')" :key="item" v-html="item" class="pb-2" />
+        </div>
         <nuxt-link :to="localePath('blog')" class="[ btn ] btn-dark [ hero__cta ] shadow">
-          {{ $t('about.card5_cta') }}
+          {{ $t('about.card5.cta') }}
         </nuxt-link>
-      </div>
-    </div>
+      </template>
+    </TwoByOne>
   </div>
 </template>
 
