a9747cbc4527a30e8921d8c3f15fdb8fe159cd57
Damien Robinson <damien.robinson@xcommedia.com.au>
2019-05-13 09:45:20 +1000

    feat(entry): format prize details as list

--- pages/win.vue
+++ pages/win.vue
@@ -1,13 +1,18 @@
 <template>
   <div class="[ main-content__left ] col-12 col-md-7">
     <div class="intro">
+      <h2>{{ $t('entry.title') }}</h2>
       <p>
-        <strong>{{ $t('entry.intro') }}</strong>
+        {{ $t('entry.intro') }}
       </p>
     </div>
 
     <div class="prize-package">
-      <p v-for="item in $t('entry.prize_package')" :key="item" v-html="item" />
+      <p>{{ $t('entry.prize_package_title') }}</p>
+      <p>{{ $t('entry.prize_package_intro') }}</p>
+      <ul>
+        <li v-for="item in $t('entry.prize_package_list')" :key="item" v-html="item" />
+      </ul>
     </div>
 
     <EntryForm />
