3884665b92d2e2e331594f39c5bc37746878a9df
Damien Robinson <damien.robinson@xcommedia.com.au>
2019-05-13 11:21:58 +1000

    feat(emailshare): remove copy above form

--- pages/emailshare.vue
+++ pages/emailshare.vue
@@ -1,16 +1,5 @@
 <template>
   <div class="[ main-content__left ] col-12 col-md-7">
-    <div class="intro">
-      <h2 v-html="$t('entry.title')" />
-      <p>
-        <strong>{{ $t('entry.intro') }}</strong>
-      </p>
-    </div>
-
-    <div class="prize-package">
-      <p>{{ $t('entry.prize-package') }}</p>
-    </div>
-
     <EmailShare />
   </div>
 </template>
