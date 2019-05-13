eb5c99b5b65ba4446dd5f6ceb304ff47ddf0c89f
Damien Robinson <damien.robinson@xcommedia.com.au>
2019-05-13 12:46:40 +1000

    feat(ondemand): add new in app page

---
+++ pages/ondemand.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <div class="intro">
+      <h2 v-html="$t('ondemand.title')" />
+      <p v-html="$t('ondemand.intro')" />
+    </div>
+    <p v-for="item in $t('ondemand.body')" :key="item" v-html="item" />
+    <p v-for="item in $t('ondemand.list')" :key="item" v-html="item" />
+    <p v-for="item in $t('ondemand.disclaimer')" :key="item" v-html="item" />
+  </div>
+</template>
