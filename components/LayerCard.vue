870e969693553702e236acd7f6a534facfcf3cb8
Damien Robinson <damien.robinson@xcommedia.com.au>
2019-05-13 17:19:07 +1000

    feat(layer card): add full width flag

--- components/LayerCard.vue
+++ components/LayerCard.vue
@@ -1,21 +1,14 @@
 <template>
   <div class="col-12 mb-5">
     <div class="row">
-      <div class="layer-card__bg col-md-8 mt-5" :class="{ 'offset-md-4': !reverse }" />
+      <div class="layer-card__bg mt-5" :class="backgroundClass" />
     </div>
 
     <div class="row">
-      <div
-        class="layer-card__bg layer-card__bg--image col-md-10 shadow"
-        :class="{ 'offset-md-2': reverse }"
-        :style="{ 'background-image': `url(${src})` }"
-      />
+      <img class="layer-card__bg layer-card__bg--image p-0" :class="imageClass" :src="src" />
     </div>
     <div class="row">
-      <div
-        class="col-lg-5 col-md-7 my-5"
-        :class="{ 'offset-lg-6': !reverse, 'offset-md-4': !reverse, 'offset-md-1': reverse }"
-      >
+      <div class="my-5" :class="cardClass">
         <div class="card card-body rounded-0 text-center shadow p-5 my-5">
           <slot />
         </div>
@@ -28,7 +21,35 @@
 export default {
   props: {
     reverse: { type: Boolean, default: false },
+    fullWidth: { type: Boolean, default: false },
     src: { type: String },
+  },
+  computed: {
+    cardClass() {
+      return {
+        'offset-lg-6': !this.reverse && !this.fullWidth,
+        'offset-md-4': !this.reverse && !this.fullWidth,
+        'offset-md-1': this.reverse && !this.fullWidth,
+        'col-lg-5': !this.fullWidth,
+        'col-md-7': !this.fullWidth,
+        'col-12': this.fullWidth,
+      };
+    },
+    imageClass() {
+      return {
+        'offset-md-2': this.reverse && !this.fullWidth,
+        'col-md-10': !this.fullWidth,
+        'col-12': this.fullWidth,
+        'position-relative': this.fullWidth,
+      };
+    },
+    backgroundClass() {
+      return {
+        'offset-md-4': !this.reverse && !this.fullWidth,
+        'col-md-8': !this.fullWidth,
+        'col-12': this.fullWidth,
+      };
+    },
   },
 };
 </script>
