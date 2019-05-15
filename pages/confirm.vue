6dab46052ceeb799ce338d7319f5dbefaca053af
Damien Robinson <damien.robinson@xcommedia.com.au>
2019-05-16 08:46:11 +1000

    refactor(pages): rename confirm page

---
+++ pages/confirm.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="[ main-content__left ] col-12 col-md-7">
+    <EntryConfirmed />
+  </div>
+</template>
+
+<script>
+import EntryConfirmed from '@/components/EntryConfirmed';
+
+export default {
+  layout: 'entry',
+  components: {
+    EntryConfirmed,
+  },
+  head() {
+    return { title: this.$t('landing.title') };
+  },
+};
+</script>
--- pages/confirmation.vue
+++ pages/confirmation.vue
@@ -1,19 +1,4 @@
-<template>
-  <div class="[ main-content__left ] col-12 col-md-7">
-    <EntryConfirmed />
-  </div>
-</template>
-
 <script>
-import EntryConfirmed from '@/components/EntryConfirmed';
-
-export default {
-  layout: 'entry',
-  components: {
-    EntryConfirmed,
-  },
-  head() {
-    return { title: this.$t('landing.title') };
-  },
-};
+import Page from '@/pages/confirm';
+export default Page;
 </script>
