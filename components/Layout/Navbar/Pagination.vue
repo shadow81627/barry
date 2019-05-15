09845280a8739d9daf1c06b49b792fa07c9c75b3
Damien Robinson <damien.robinson@xcommedia.com.au>
2019-05-15 11:39:48 +1000

    fix(pagination): exact link

--- components/Layout/Navbar/Pagination.vue
+++ components/Layout/Navbar/Pagination.vue
@@ -3,7 +3,7 @@
     class="container-break-out-center my-4 justify-content-center pagination-wrapper d-none d-md-flex"
   >
     <div v-for="link in $t('pagination')" :key="link.value" class="col-md-auto col-6">
-      <b-nav-item :to="localePath(link.value)" link-classes="p-0" active-class="active">
+      <b-nav-item :to="localePath(link.value)" link-classes="p-0" active-class="active" exact>
         <i class="fa fa-circle" aria-hidden="true" />
         {{ link.text }}
       </b-nav-item>
