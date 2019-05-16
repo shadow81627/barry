bd0f99c4c4b66f4b52f04a9184990a704d141df3
Damien Robinson <damien.robinson@xcommedia.com.au>
2019-05-16 16:34:16 +1000

    fix(footer): padding on rows

--- components/Layout/Footer.vue
+++ components/Layout/Footer.vue
@@ -2,7 +2,7 @@
   <footer class="bg-dark text-light pt-4 pb-4">
     <div class="container">
       <div class="row justify-content-center align-items-center text-center">
-        <div class="col-12 pb-4">
+        <div class="col-12">
           <img
             class="img-fluid d-block mx-auto"
             src="@/assets/images/QA_ScUber_WR.png"
@@ -10,13 +10,13 @@
           />
         </div>
 
-        <div class="col-12 py-2"><LangSelect class="col-md-4 col-sm-6 col-12" /></div>
+        <div class="col-12 py-4"><LangSelect class="col-md-4 col-sm-6 col-12" /></div>
 
-        <div v-for="link in $t('footer.links')" :key="link.text" class="col-12 py-2">
+        <div v-for="link in $t('footer.links')" :key="link.text" class="col-12 py-2 d-sm-none">
           <a
             :href="link.href"
             target="_blank"
-            class="btn btn-outline-light col-md-4 col-sm-6 col-12 d-sm-none"
+            class="btn btn-outline-light col-md-4 col-sm-6 col-12"
           >
             {{ link.text }}
           </a>
