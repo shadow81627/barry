8877b05ee6cddece87cd7692203617e04ca2d4a7
Damien Robinson <damien.robinson@xcommedia.com.au>
2019-05-16 14:56:12 +1000

    fix(lang select): use select drop down

--- components/Layout/Navbar/LangSelect.vue
+++ components/Layout/Navbar/LangSelect.vue
@@ -1,55 +1,35 @@
 <template>
-  <b-nav-item-dropdown class="lang badge badge-pill badge-grey p-0">
-    <template slot="button-content">
-      <span class="current-lang">{{ currentLocale().name }}</span>
-    </template>
-    <b-dropdown-item-button
+  <b-select v-model="locale" class="lang">
+    <option :value="currentLocale.code">{{ currentLocale.name }}</option>
+    <option
       v-for="locale in $i18n.locales.filter(i => i.code !== $i18n.locale)"
       :key="locale.code"
+      :value="locale.code"
+      >{{ locale.name }}</option
     >
-      <nuxt-link :to="switchLocalePath(locale.code)">
-        <span style="color:black;">{{ locale.name }}</span>
-      </nuxt-link>
-    </b-dropdown-item-button>
-  </b-nav-item-dropdown>
+  </b-select>
 </template>
 
 <script>
-const images = require.context('@/assets/images/flags', false, /\.png$/);
-
 export default {
-  methods: {
+  computed: {
     currentLocale() {
       return this.$i18n.locales.find(i => i.code === this.$i18n.locale);
     },
-    flagUrl(flag) {
-      let image = '';
-      try {
-        image = images(`./${flag}.png`);
-      } catch (e) {
-        console.log(e);
-      }
-
-      return image;
+    locale: {
+      get() {
+        return this.$i18n.locales.find(i => i.code === this.$i18n.locale).code;
+      },
+      set(value) {
+        this.$router.push(this.switchLocalePath(value));
+      },
     },
   },
 };
 </script>
 
 <style>
-.lang .current-lang {
-  color: black;
-}
-
-.badge-grey {
-  color: #212529;
-  background-color: #f8f9fa;
-  opacity: 0.9;
-}
-
-.lang .dropdown-toggle::after {
-  vertical-align: 0;
-  font-size: 1.25rem;
-  color: grey;
+.lang.lang {
+  border-radius: 10rem;
 }
 </style>
