8057f2fe6e5765850d45e7e5ccc0cb69fd47f4f0
Damien Robinson <damien.robinson@xcommedia.com.au>
2019-05-17 17:26:28 +1000

    feat(confirm): update confirm page layout to default

--- pages/confirm.vue
+++ pages/confirm.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="[ main-content__left ] col-12 col-md-7">
+  <div class="[ main-content__left ] col-12 col-md-8 offset-md-2 p-0">
     <EntryConfirmed />
   </div>
 </template>
@@ -8,12 +8,20 @@
 import EntryConfirmed from '@/components/EntryConfirmed';
 
 export default {
-  layout: 'entry',
+  // layout: 'entry',
   components: {
     EntryConfirmed,
   },
   head() {
-    return { title: this.$t('landing.title') };
+    return {
+      link: [
+        {
+          rel: 'stylesheet',
+          href:
+            'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css',
+        },
+      ],
+    };
   },
 };
 </script>
