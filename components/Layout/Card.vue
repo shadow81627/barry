d18c98dcb7fc7849d97e4871becf6cc165dd3576
Damien Robinson <damien.robinson@xcommedia.com.au>
2019-05-17 12:01:36 +1000

    fix(card): remove background color

--- components/Layout/Card.vue
+++ components/Layout/Card.vue
@@ -1,16 +1,16 @@
 <template>
   <div class="__card">
-    <div class="card__item bg-secondary">
+    <div class="card__item">
       <div class="card__content imagecardone">
         <b-img-lazy src="/images/win1-image.jpg" alt="scuber" />
       </div>
     </div>
-    <div class="card__item bg-secondary">
+    <div class="card__item">
       <div class="card__content imagecardone">
         <b-img-lazy src="/images/win2-image.jpg" alt="scuber" />
       </div>
     </div>
-    <div class="card__item bg-secondary">
+    <div class="card__item">
       <div class="card__content imagecardone">
         <b-img-lazy src="/images/win3-image.jpg" alt="scuber" />
       </div>
