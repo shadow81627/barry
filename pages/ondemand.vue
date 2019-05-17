aed6cfdeabf1fea2f4afa7c7fe62af9473b4752e
Damien Robinson <damien.robinson@xcommedia.com.au>
2019-05-17 14:11:05 +1000

    feat(ondeamand): add sections

--- pages/ondemand.vue
+++ pages/ondemand.vue
@@ -5,7 +5,10 @@
       <p v-html="$t('ondemand.intro')" />
     </div>
     <p v-for="item in $t('ondemand.body')" :key="item" v-html="item" />
-    <!-- <p v-for="item in $t('ondemand.list')" :key="item" v-html="item" /> -->
+    <div v-for="section in $t('ondemand.sections')" :key="section.title">
+      <h2 v-html="section.title" />
+      <p v-for="item in section.list" :key="item" v-html="item" />
+    </div>
     <p v-for="item in $t('ondemand.disclaimer')" :key="item" v-html="item" />
   </div>
 </template>
