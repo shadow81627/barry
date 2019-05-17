f68e1483dd8095599ec18efc1dace4c462619fc7
Damien Robinson <damien.robinson@xcommedia.com.au>
2019-05-17 14:51:58 +1000

    feat(ondemand): add map images

--- pages/ondemand.vue
+++ pages/ondemand.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div class="intro">
-      <h2 v-html="$t('ondemand.title')" />
+      <h2 v-html="$t('ondemand.heading')" />
       <p v-html="$t('ondemand.intro')" />
     </div>
     <p v-for="item in $t('ondemand.body')" :key="item" v-html="item" />
@@ -9,7 +9,20 @@
       <h2 v-html="section.title" />
       <p v-for="item in section.list" :key="item" v-html="item" />
     </div>
-    <p v-for="item in $t('ondemand.disclaimer')" :key="item" v-html="item" />
+    <!-- map section -->
+    <div>
+      <h2 v-html="$t('ondemand.map.title')" />
+      <div class="row">
+        <div class="col-sm-6">
+          <b-img-lazy :src="$t('ondemand.map.image1')" fluid />
+        </div>
+        <div class="col-sm-6">
+          <b-img-lazy :src="$t('ondemand.map.image2')" fluid />
+        </div>
+      </div>
+    </div>
+    <!-- disclaimer -->
+    <small v-for="item in $t('ondemand.disclaimer')" :key="item" v-html="item" />
   </div>
 </template>
 
