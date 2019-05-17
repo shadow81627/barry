79b15e60526270a30fcf78a73276e283032576e7
Damien Robinson <damien.robinson@xcommedia.com.au>
2019-05-17 15:00:02 +1000

    feat(pages): rename ondemand to request

---
+++ pages/request.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <div class="intro">
+      <h2 v-html="$t('ondemand.heading')" />
+      <p v-html="$t('ondemand.intro')" />
+    </div>
+    <p v-for="item in $t('ondemand.body')" :key="item" v-html="item" />
+    <div v-for="section in $t('ondemand.sections')" :key="section.title">
+      <h2 v-html="section.title" />
+      <p v-for="item in section.list" :key="item" v-html="item" />
+    </div>
+    <!-- map section -->
+    <div>
+      <h2 v-html="$t('ondemand.map.title')" />
+      <div class="row">
+        <div class="col-sm-6">
+          <b-img-lazy :src="$t('ondemand.map.image1')" fluid />
+        </div>
+        <div class="col-sm-6">
+          <b-img-lazy :src="$t('ondemand.map.image2')" fluid />
+        </div>
+      </div>
+    </div>
+    <!-- disclaimer -->
+    <small v-for="item in $t('ondemand.disclaimer')" :key="item" v-html="item" />
+  </div>
+</template>
+
+<script>
+export default {
+  layout: 'ondemand',
+};
+</script>
