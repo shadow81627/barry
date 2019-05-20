fefd4f21e115ffb99fd7ae6bea8f4f637a650740
Damien Robinson <damien.robinson@xcommedia.com.au>
2019-05-20 11:40:54 +1000

    feat(deals): rename deals to offers

---
+++ pages/offers.vue
@@ -0,0 +1,47 @@
+<template>
+  <Section v-if="sections" :sections="sections" />
+</template>
+
+<script>
+import utils from '@/assets/js/utils';
+
+import Section from '@/components/Deal/Section';
+
+export default {
+  layout: 'deals',
+  components: {
+    Section,
+  },
+  data: function() {
+    return {
+      sections: this.$t('deals.sections'),
+    };
+  },
+  computed: {
+    sortDisabledDeals: function() {
+      return this.sections;
+      // return [...this.sections].sort(function(x, y) {
+      //   // true values first
+      //   return x === y ? 0 : x ? -1 : 1;
+      //   // false values first
+      //   // return x === y ? 0 : x ? 1 : -1;
+      // });
+    },
+  },
+  methods: {
+    chunk: utils.chunk,
+  },
+};
+</script>
+
+<style>
+@media only screen and (max-width: 429px) {
+  .deal-column.deal-column {
+    padding: 0px;
+  }
+
+  .deal__description.deal__description {
+    margin: 0px !important;
+  }
+}
+</style>
