4d7ed9c2ca9633aff95a63cb74e5319a7d0e1051
Damien Robinson <damien.robinson@xcommedia.com.au>
2019-05-20 17:32:33 +1000

    feat(card): remove and move into min right content

--- components/Layout/MainContentRight.vue
+++ components/Layout/MainContentRight.vue
@@ -1,15 +1,11 @@
 <template>
   <div class="main-content__right">
-    <card />
+    <div class="__card" v-for="item in $t('mrec-tiles')" :key="item.alt">
+      <div class="card__item">
+        <div class="card__content imagecardone">
+          <b-img-lazy :src="item.image" :alt="item.alt" />
+        </div>
+      </div>
+    </div>
   </div>
 </template>
-
-<script>
-import Card from './Card';
-
-export default {
-  components: {
-    Card,
-  },
-};
-</script>
