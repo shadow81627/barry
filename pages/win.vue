f61c79c778e440bee9d97e54b18f3fa598d3a64b
Damien Robinson <damien.robinson@xcommedia.com.au>
2019-05-20 14:49:38 +1000

    feat(win): prize dot points

--- pages/win.vue
+++ pages/win.vue
@@ -9,11 +9,11 @@
 
     <div class="prize-package">
       <h2 class="h4 text-primary">{{ $t('entry.prize_package_title') }}</h2>
-      <ul class="color-list" style="padding-inline-start: 0px;">
+      <ul class="color-list" style="xpadding-inline-start: 0px;">
         <li
           v-for="item in $t('entry.prize_package_list')"
           :key="item"
-          style="padding-left: 4rem;"
+          style="xpadding-left: 4rem;"
           v-html="item"
         />
       </ul>
