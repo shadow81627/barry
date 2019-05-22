85f499be745c52e2716e7de174926158052d8f9d
Damien Robinson <damien.robinson@xcommedia.com.au>
2019-05-22 16:25:19 +1000

    feat(translate): errors

--- components/error/404.vue
+++ components/error/404.vue
@@ -1,8 +1,9 @@
 <template>
   <div class="container">
-    <h1>Page not found</h1>
-    <nuxt-link to="/">
-      Home
+    <h1>{{ $t('error.404.heading') }}</h1>
+    <p v-for="item in $t('error.404.body')" :key="item" v-html="item" />
+    <nuxt-link :to="localePath($t('error.404.cta.path'))">
+      {{ $t('error.404.cta.text') }}
     </nuxt-link>
   </div>
 </template>
--- components/error/500.vue
+++ components/error/500.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="container">
+    <h1>{{ $t('error.500.heading') }}</h1>
+    <p v-for="item in $t('error.500.body')" :key="item" v-html="item" />
     <h1>An unexpected error has occurred</h1>
     <p>
       <span>Please <a href="javascript:location.reload();">try again</a>, or email </span>
