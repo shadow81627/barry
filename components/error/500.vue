57753a9139b0a9294e602c13a2a4c3eeb59cabd1
Damien Robinson <damien.robinson@xcommedia.com.au>
2019-05-22 17:57:11 +1000

    fix(500): remove old copy

--- components/error/500.vue
+++ components/error/500.vue
@@ -2,14 +2,6 @@
   <div class="container">
     <h1>{{ $t('error.500.heading') }}</h1>
     <p v-for="item in $t('error.500.body')" :key="item" v-html="item" />
-    <h1>An unexpected error has occurred</h1>
-    <p>
-      <span>Please <a href="javascript:location.reload();">try again</a>, or email </span>
-      <a href="mailto:dev@xcommedia.com.au?subject=Technical%20issue">
-        dev@xcommedia.com.au
-      </a>
-      <span> with the words "<strong>Technical issue</strong>" in the subject line.</span>
-    </p>
   </div>
 </template>
 <script>
