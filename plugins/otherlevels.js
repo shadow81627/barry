853a99a3eee17af718a1d1a3dfaeac95ffc9981d
Damien Robinson <damien.robinson@xcommedia.com.au>
2019-05-22 18:47:33 +1000

    fix(otherlevels): current page

--- plugins/otherlevels.js
+++ plugins/otherlevels.js
@@ -1,5 +1,11 @@
+// import axios from 'axios';
+
+// import EventBus from '@/assets/js/EventBus.js';
+
 export default ({ app }, inject) => {
-  function olSetup() {
+  console.log(app);
+  function olSetup(page) {
+    console.log(`page: ${page}`);
     // Application/Project Key. Available from Projects screen in Portal.
     const appKey = '7b9f8bc23f715afc5141842400f6451a';
     const options = {
@@ -12,33 +18,36 @@ export default ({ app }, inject) => {
       },
     };
 
-    const olMaxDepth = 3;
+    // const olMaxDepth = 3;
     const olPageUrlArray = location.pathname
       .substring(location.pathname.indexOf('/') + 1)
       .split('/');
-    console.log(this);
     console.log(olPageUrlArray);
-    let olCurrentPageName;
+    const olCurrentPageName = page;
 
     // grab the URL relative path and dissect it for use in tags and events
-    const olLastPageCategoryTags = olPageUrlArray
-      .slice(0, olMaxDepth)
-      .filter(function(url, index) {
-        return url && url.length;
-      })
-      .map(function(url, index) {
-        return {
-          name: 'lastPageCategory' + (index + 1),
-          value: olPageUrlArray[index] || '',
-          type: 'string',
-        };
-      });
+    // const olLastPageCategoryTags = olPageUrlArray
+    //   .slice(0, olMaxDepth)
+    //   .filter(function(url, index) {
+    //     return url && url.length;
+    //   })
+    //   .map(function(url, index) {
+    //     return {
+    //       name: 'lastPageCategory' + (index + 1),
+    //       value: olPageUrlArray[index] || '',
+    //       type: 'string',
+    //     };
+    //   });
 
-    if (olLastPageCategoryTags.length) {
-      olCurrentPageName = olPageUrlArray[0];
-    } else {
-      olCurrentPageName = 'home';
-    }
+    // if (olLastPageCategoryTags.length) {
+    //   olCurrentPageName = olPageUrlArray[0];
+    //   console.log(route);
+    //   console.log(olCurrentPageName);
+    // } else {
+    //   olCurrentPageName = route.path;
+    //   console.log(route);
+    //   console.log(olCurrentPageName);
+    // }
 
     _ol('create', appKey, options, function() {
       if (window.localStorage) {
@@ -83,7 +92,8 @@ export default ({ app }, inject) => {
         if (isSupported) {
           _ol('push.isSubscribed', function(isSubscribed) {
             if (!isSubscribed) {
-              if (olCurrentPageName !== 'win') {
+              console.log(`Push supported: ${page}`);
+              if (!olCurrentPageName.includes('win')) {
                 setTimeout(
                   _ol('askForPermission', 'notification', {}, function(subscribed) {
                     if (subscribed === 'subscribed') {
@@ -102,7 +112,59 @@ export default ({ app }, inject) => {
         }
       });
 
-      if (olCurrentPageName !== 'win') {
+      // const submitForm = _ol('getTag', 'submitForm', function(tag) {
+      //   return tag.value;
+      // });
+      // console.log(`submitForm: ${submitForm}`);
+
+      // const TEQOptIn = _ol('getTag', 'TEQOptIn', function(tag) {
+      //   return tag.value;
+      // });
+
+      // console.log(`TEQOptin: ${TEQOptIn}`);
+
+      // if (submitForm && TEQOptIn === 0) {
+      //   // subscriber here
+      //   console.log('UpdateTEQOptin');
+      //   const url = `${process.env.API_BASE_URL}/api/teq_opt_in`;
+      //   axios
+      //     .post(url, { secret: 'secret', hash: 'some hash' })
+      //     .then(response => {
+      //       console.log(response);
+
+      //       _ol('setTag', 'updateTEQOptIn', 1, 'numeric', function() {});
+
+      //       // Trigger event
+      //       EventBus.$emit('entry-confirmed', true);
+      //       EventBus.$emit('notification', {
+      //         type: 'success',
+      //         message: 'Subscribe successful.',
+      //       });
+      //       this.$router.push(this.localePath('confirm'));
+      //     })
+      //     .catch(error => {
+      //       EventBus.$emit('notification', {
+      //         type: 'error',
+      //         message: 'Error submitting update.',
+      //       });
+      //       if (error.response) {
+      //         // The request was made and the server responded with a status code
+      //         // that falls out of the range of 2xx
+      //         console.log(error.response.data);
+      //         console.log(error.response.status);
+      //         console.log(error.response.headers);
+      //       } else if (error.request) {
+      //         console.log(error.request);
+      //       } else {
+      //         // Something happened in setting up the request that triggered an Error
+      //         console.log('Error', error.message);
+      //       }
+      //       console.log(error.config);
+      //     });
+      // }
+
+      if (!olCurrentPageName.includes('win')) {
+        console.log(`olSubscribed: ${page}`);
         const olSubscribed = function(event, context) {
           context.close();
           context.pushPhash();
