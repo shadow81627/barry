7e4a5bec9445a4784fdb4117f47a6b9f81adb9d8
Damien Robinson <damien.robinson@xcommedia.com.au>
2019-05-30 16:53:07 +1000

    refactor(close): middleware no longer check close date

--- middleware/competition-state.js
+++ middleware/competition-state.js
@@ -1,19 +1,3 @@
 export default function({ app, env, redirect }) {
-  // Check competition state
-  // const openTime = new Date(env.COMPETITON_OPEN_TIME);
-  const closeTime = new Date(env.COMPETITON_CLOSE_TIME);
-  const now = new Date();
-
-  console.log(`now: ${now}`);
-  console.log(`close: ${closeTime}`);
-
-  // comming soon
-  // if (now <= openTime) {
-  //   return app.router.push(app.localePath('comingsoon'));
-  // }
-
-  // closed
-  if (now >= closeTime) {
-    return redirect(app.localePath('index'));
-  }
+  return redirect(app.localePath('index'));
 }
