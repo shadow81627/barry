d1f71670d90bf278aea06e71d9c4c8a49c81893c
Damien Robinson <damien.robinson@pocketpasta.com>
2021-07-17 10:48:45 +1000

    chore: generate source map

--- nuxt.config.js
+++ nuxt.config.js
@@ -184,15 +184,9 @@ export default {
     /*
      ** You can extend webpack config here
      */
-    extend(config, ctx) {
-      // Run ESLint on save
-      if (ctx.isDev && ctx.isClient) {
-        config.module.rules.push({
-          enforce: 'pre',
-          test: /\.(js|vue)$/,
-          loader: 'eslint-loader',
-          exclude: /(node_modules)/,
-        });
+    extend(config, { isClient }) {
+      if (isClient) {
+        config.devtool = 'source-map';
       }
     },
   },
