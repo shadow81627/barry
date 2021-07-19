8b0850dc1e9555f764d6cf6dc3279a2d78df2fe8
Damien Robinson <damien.robinson@pocketpasta.com>
2021-07-19 10:27:16 +1000

    test: add all pages

--- tests/e2e/base_test.js
+++ tests/e2e/base_test.js
@@ -1,5 +1,13 @@
 const pages = [
   { slug: 'home', heading: 'Meet scUber', route: '/' },
+  { slug: 'close' },
+  { slug: 'confirm' },
+  { slug: 'commingsoon', route: '/comingsoon/index.html' },
+  { slug: 'emailshare' },
+  { slug: 'explore' },
+  { slug: 'offers' },
+  { slug: 'request' },
+  { slug: 'unsubscribe' },
   { slug: 'win', heading: 'WIN A SCUBER RIDE' },
 ];
 
@@ -8,9 +16,12 @@ for (const { slug, heading, route } of pages) {
 
   Scenario('Heading 1', async ({ I }) => {
     I.amOnPage(route || `/${slug}`);
-    I.see(heading, 'h1');
-    I.wait(5);
 
+    if (heading) {
+      I.see(heading, 'h1');
+    }
+
+    I.wait(5);
     I.saveScreenshot(`${slug}.png`);
     await I.seeVisualDiff(`${slug}.png`);
   });
